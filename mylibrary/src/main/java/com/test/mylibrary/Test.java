37bc752466e623db19678ff17c68f0447d51c41d
begumbeyzakocahuyuk <begumbkocahuyuk@gmail.com>
2021-12-01 16:36:00 +0300

    First Commit

---
+++ mylibrary/src/main/java/com/test/mylibrary/Test.java
@@ -0,0 +1,10 @@
+package com.test.mylibrary;
+
+import android.util.Log;
+
+public class Test {
+    private static final String TAG="Test Basarili";
+    public static void d(String message){
+        Log.d(TAG,message);
+    }
+}
